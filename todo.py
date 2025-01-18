280342784616a5779c98295fd704c71c6f276ac6
Zerubbabel <thundervoltatbolt@gmail.com>
2025-01-18 06:05:10 +0300

    loading todos

--- todo.py
+++ todo.py
@@ -8,5 +8,9 @@ class TodoList:
     def __init__(self, filename=TODO_FILE):
         self.filename = filename
         self.todos = self.load_todos()
-    
-
+    # loading the json file
+    def load_todos(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                return json.load(file)
+        return []
