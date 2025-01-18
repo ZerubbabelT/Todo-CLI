e7cc5166833f31371a958c2625702f89e69fa3ce
Zerubbabel <thundervoltatbolt@gmail.com>
2025-01-18 06:00:05 +0300

    create todo_list.json & initialized TodoList class

--- todo.py
+++ todo.py
@@ -1,1 +1,12 @@
+import os
+import sys
+import json
+
+TODO_FILE = "todo_list.json"
+
+class TodoList:
+    def __init__(self, filename=TODO_FILE):
+        self.filename = filename
+        self.todos = self.load_todos()
+    
 
